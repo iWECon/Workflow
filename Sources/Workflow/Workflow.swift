cfe5beff925fb9d753d4758f8a988b25a16a791d
iWe <iwecon@aol.com>
2022-04-04 03:22:40 +0800

    Support Concurrency; add tests

--- Sources/Workflow/Workflow.swift
+++ Sources/Workflow/Workflow.swift
@@ -1,8 +1,4 @@
-#if os(macOS)
-import AppKit
-#elseif os(iOS)
-import UIKit
-#endif
+import Foundation
 
 public protocol Thenable {
     associatedtype T
@@ -11,9 +7,9 @@ public protocol Thenable {
 
 public extension Thenable {
     
-    func then<U>(_ then: (T) throws -> U) rethrows -> Workflow<U> {
+    func then<U>(_ then: (T) throws -> U) throws -> Workflow<U> {
         let v = try then(value)
-        return Workflow { v }
+        return try Workflow { v }
     }
     
     func end(_ end: (T) throws -> Void) throws -> Void {
@@ -23,11 +19,36 @@ public extension Thenable {
     func done() throws -> Void { }
 }
 
+#if compiler(>=5.5) && canImport(_Concurrency)
+@available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
+public extension Thenable {
+    
+    func then<U>(_ then: (T) async throws -> U) async throws -> Workflow<U> {
+        let v = try await then(value)
+        return try Workflow { v }
+    }
+    
+    func end(_ end: (T) async throws -> Void) async throws -> Void {
+        try await end(value)
+    }
+    
+    func done() async throws -> Void { }
+    
+}
+#endif
+
 public struct Workflow<Element>: Thenable {
     public typealias T = Element
     public var value: Element
     
-    public init(_ block: () throws -> Element) rethrows {
+    public init(_ block: () throws -> Element) throws {
         self.value = try block()
     }
+    
+    #if compiler(>=5.5) && canImport(_Concurrency)
+    @available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
+    public init(_ block: () async throws -> Element) async throws {
+        self.value = try await block()
+    }
+    #endif
 }
--- Tests/WorkflowTests/WorkflowTests.swift
+++ Tests/WorkflowTests/WorkflowTests.swift
@@ -6,5 +6,33 @@ final class WorkflowTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
+        
+        let w1 = try Workflow {
+            "Workflow"
+        }.then {
+            $0 + " yes!"
+        }.value
+        
+        XCTAssertEqual(w1, "Workflow yes!")
+        
+        try Workflow {
+            "Workflow"
+        }.then {
+            $0 + " yes!"
+        }.end {
+            XCTAssertEqual($0, "Workflow yes!")
+        }
+    }
+    
+    @available(macOS 12.0, *)
+    func testConcurrency() async throws {
+        
+        try await Workflow {
+            try await URLSession.shared.data(for: URLRequest(url: URL(string: "https://www.google.com/ncr")!))
+        }.then({ pair in
+            pair.0.isEmpty
+        }).end({ value in
+            XCTAssertEqual(value, false)
+        })
     }
 }
